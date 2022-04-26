08ba1a381b3104cf23768637b2bb9485a235da3a
Ziyu Huang <ziyu4huang@gmail.com>
2022-04-27 05:41:29 +0800

    Add test run on Tensorflow 1.15.0 / directml  / WSL2

--- ray_rllib/ray_rllib_try_taxi_v3.py
+++ ray_rllib/ray_rllib_try_taxi_v3.py
@@ -38,6 +38,7 @@ trainer = PPOTrainer(config=config)
 # loss calculation on the collected batch and a model update.
 for _ in range(3):
     # this will crash in TF2.8 / Python 3.7.13 / Ubuntu[CPU]
+    # this will OK in TF1.15 / Python 3.7.13 / Ubuntu[GPU/DirectML]
     # but it's pretty ok in MacBook Air M1 Setup / with TF2.8/ Python 3.9.5 /MacOS 12.3.1[GPU]
     print(trainer.train())
 
